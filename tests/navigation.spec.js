2f9acd70df3d646a669df38896094c013eea36db
Madelene <madelenetrapp@outlook.com>
2026-06-02 15:51:38 +0200

    lägger till första röda e2e-testet för navigation mellan vyer

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -3,3 +3,16 @@ import { test, expect } from '@playwright/test'
 test('kan navigera till mina böcker', async ({ page }) => {
   await page.goto('https://ha-fed25-testning.github.io/exam_e2e/')
 })
+
+test('kan navigera mellan vyerna', async ({ page }) => {
+
+  await page.getByRole('button', { name: 'Katalog' }).click()
+  await expect(page.getByText('Välkommen!')).toBeVisible()
+
+  await page.getByRole('button', { name: 'Lägg till bok' }).click()
+  await expect(page.getByText('Titel')).toBeVisible()
+
+  await page.getByRole('button', { name: 'Mina böcker' }).click()
+  await expect(page.getByText('Mina böcker')).toBeVisible()
+
+})
